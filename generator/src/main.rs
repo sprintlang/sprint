32e5edec13deab1dcee9379e3ddb355ed16e04b6
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2019-10-09 22:58:02 +0100

    Create MoveVisitor and implement Visitor trait

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,4 +1,26 @@
+use sprint_parser::ast::contract::Visitor;
+
+struct MoveVisitor {}
+
+impl MoveVisitor {
+    fn new() -> MoveVisitor {
+        MoveVisitor {}
+    }
+}
+
+impl Visitor for MoveVisitor {
+    fn visit_zero(&mut self) {
+        // The empty contract.
+    }
+
+    fn visit_one(&mut self) {
+        println!("Visting one primitive");
+    }
+}
+
 fn main() {
     let move_one_code = include!("./sample_move_program.rs");
     println!("My Move code is: {}", move_one_code);
+    let mut visitor: MoveVisitor = MoveVisitor::new();
+    visitor.visit_one();
 }
