24d0baf22d6b5f500e2a4f210727d7798d53158e
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2019-10-11 15:36:49 +0100

    Make comments more Rustdoc friendly

--- generator/src/move_visitor.rs
+++ generator/src/move_visitor.rs
@@ -4,13 +4,13 @@ static MOVE_ONE_CODE: &str = include_str!("./move_one_contract.mvir");
 
 #[derive(Default)]
 pub struct MoveVisitor {
-    move_code: String, // Accumulates Move code.
+    /// Accumulates Move code.
+    move_code: String,
 }
 
 impl Visitor for MoveVisitor {
-    fn visit_zero(&mut self) {
-        // The empty contract.
-    }
+    /// The empty contract.
+    fn visit_zero(&mut self) {}
 
     fn visit_one(&mut self) {
         self.move_code.push_str(MOVE_ONE_CODE);
