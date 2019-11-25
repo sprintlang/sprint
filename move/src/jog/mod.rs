23897b2d2ee64de43532160af5e8c4619f4df80c
Pablo Gamito <pablo@gamito.me>
2019-11-25 17:31:05 +0000

    Add transition command to cli to move between contract states

--- move/src/jog/mod.rs
+++ move/src/jog/mod.rs
@@ -2,4 +2,5 @@ pub mod action;
 pub mod expression;
 pub mod method;
 pub mod module;
+pub mod script;
 pub mod variable;
--- move/src/lib.rs
+++ move/src/lib.rs
@@ -4,6 +4,8 @@ mod visitor;
 use self::visitor::State;
 use sprint_parser::ast::state as ast;
 
+pub use jog::script;
+
 pub fn generate(state: &ast::State) -> String {
     let mut visitor = State::default();
     visitor.visit(state);
